4ebc745a7be12c6d4100de72f0ec86e136b017ad
lohi212 <suvvarilohita@gmail.com>
2023-12-06 18:55:24 +0530

    feat: auth wrapper

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,8 @@
+import { useAuthenticator } from "@aws-amplify/ui-react";
+
 const Home = () => {
+  const { user, signOut } = useAuthenticator((context) => [context.user]);
+  console.log({ user });
   return (
     <div>
       <h1>Home Page</h1>
